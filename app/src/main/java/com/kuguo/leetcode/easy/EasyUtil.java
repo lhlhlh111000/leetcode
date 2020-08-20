32d1dbee6968337ca0c17be614fecc303389bc73
lhlhlh111000 <zlh111000@gmail.com>
2020-08-20 10:17:55 +0800

    levelOrderBottom

--- app/src/main/java/com/kuguo/leetcode/easy/EasyUtil.java
+++ app/src/main/java/com/kuguo/leetcode/easy/EasyUtil.java
@@ -2,9 +2,13 @@ package com.kuguo.leetcode.easy;
 
 import com.kuguo.leetcode.model.TreeNode;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
+import java.util.Queue;
 import java.util.Stack;
 
 /**
@@ -403,5 +407,29 @@ public class EasyUtil {
             int rightMax = maxDepth(root.right);
             return Math.max(leftMax, rightMax) + 1;
         }
+    }
+
+    public static List<List<Integer>> levelOrderBottom(TreeNode root) {
+        if (root == null) return new ArrayList<>();
+        List<List<Integer>> ans = new LinkedList<>();//使用有序列表
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> list = new ArrayList<>();
+            while (size > 0) {
+                TreeNode node = queue.poll();
+                list.add(node.val);
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+                size--;
+            }
+            ans.add(0, list);
+        }
+        return ans;
     }
 }
