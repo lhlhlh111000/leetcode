a2d87f6857d4f3b13933b131fc5807b3006e4292
lhlhlh111000 <zlh111000@gmail.com>
2020-08-31 09:35:41 +0800

    getIntersectionNode

--- app/src/main/java/com/kuguo/leetcode/easy/EasyUtil.java
+++ app/src/main/java/com/kuguo/leetcode/easy/EasyUtil.java
@@ -581,4 +581,13 @@ public class EasyUtil {
 
         return true;
     }
+
+    public static ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode ha = headA, hb = headB;
+        while (ha != hb) {
+            ha = ha != null ? ha.next : headB;
+            hb = hb != null ? hb.next : headA;
+        }
+        return ha;
+    }
 }
